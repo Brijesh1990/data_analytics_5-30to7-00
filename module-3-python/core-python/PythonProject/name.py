47a319eddafd6b584bbc71a3c83f792892006724
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-02-21 19:00:56 +0530

    21-02-2026 all data uploaded

--- module-3-python/core-python/PythonProject/name.py
+++
@@ -1,6 +0,0 @@
-# a=10
-# b=20
-# c=a+b
-# print("additions of numbers is :",c)
-
-
--- module-3-python/core-python/name.py
+++ module-3-python/core-python/name.py
@@ -1,4 +1,2 @@
-# w.a.p to print your name take input from user
-name=input('Enter your name :')
-# print(name)
-print("my name is :",name)
+name="i am Het kumar"
+print(name)
