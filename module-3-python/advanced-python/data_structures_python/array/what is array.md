9f4ce1350492c2e53c9d8d701fc0277187cacb02
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-04-16 18:41:41 +0530

    16-04-2026 all data updated

---
+++ module-3-python/advanced-python/data_structures_python/array/what is array.md
@@ -0,0 +1,29 @@
+# what is array ?
+
+ 1. array is not used as any data types in python 
+ 2. array is stored multiple data in a single variables 
+ 3. array is mutable 
+ 4. list used in python as array 
+
+
+ **examples of array**
+ ```
+ emp=["brijesh","kumar","het"]
+ print(emp)
+ print(emp[1]) 
+
+ ```
+
+# numpy in python  :
+
+  1. numpy are used in python as a library
+  2. numpy used to create array 
+  3. numpy is used to create array | 2d array | multidimentional array 
+  4. numpy used to install pip install numpy 
+
+  ```
+  import numpy as np
+  res=np.array([1,2,3,4,5,6])
+  print(res)
+  
+  ```
---
+++ module-3-python/advanced-python/data_structures_python/sets/what is set.py
@@ -0,0 +1,16 @@
+# what is set in python ?
+# set is an unordered data structured data types in python
+# set is used to stored data in not any ordered or key:values formate
+# set is also used as mutable
+# set is also used to stored any data types shuld be : string | tuple | dictionary | list
+# set is changed | add | update | delete items
+# set is also defined inside of {} without key:values paired
+# set used inbuilt method set() to convert any data types into set()
+
+employee={"divyang","brijesh","het"}
+print(employee)
+# check set datatypes
+print(type(employee))
+# accesing set elements
+print(set(employee))
+
